45c925243471d48479610fc3939ad1f1b09f6ba0
Bard Avelia <a.bard@rubika-edu.com>
2025-12-15 15:12:15 +0100

    Helper

---
+++ Assets/com.avelia.utils/Helper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace com.avelia.utils
+{
+    public static class Helpers
+    {
+        public static void meow()
+        {
+            Debug.Log("meowwww");
+        }
+
+    }
+}
+
